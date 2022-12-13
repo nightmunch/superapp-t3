f956468689b9093fb95d1d9ad374ff192d663123
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2022-12-13 19:57:46 +0800

    forgot about claim lol

--- src/pages/moneytrack/claim.tsx
+++ src/pages/moneytrack/claim.tsx
@@ -13,15 +13,18 @@ import { z } from "zod";
 import toast from "react-hot-toast";
 import { trpc } from "../../utils/trpc";
 import type { Claim as ClaimsProps } from "@prisma/client";
+import { useSession } from "next-auth/react";
 
 const Claim: NextPage = () => {
+  const { data: sessionData } = useSession();
+
   const [selectedID, setSelectedID] = useState<string>("");
   const [handleAddModal, setHandleAddModal] = useState(false);
   const [handleDeleteModal, setHandleDeleteModal] = useState(false);
   const [handleShowModal, setHandleShowModal] = useState(false);
 
   const claims = trpc.claim.listall.useQuery({
-    userId: "cl5qwgu6k0015zwv8jt19n94s",
+    userId: sessionData?.user?.id ?? "cl5qwgu6k0015zwv8jt19n94s",
   });
 
   const addClaims = trpc.claim.create.useMutation({
