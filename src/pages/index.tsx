a95b9f1545d2945c49071cd4499a6e49497edba3
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2022-12-18 21:50:18 +0800

    attempt better?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -77,66 +77,71 @@ const LoopingBar = () => {
 };
 
 const Clock = () => {
-  const [hour, setHour] = useState(0);
-  const [minute, setMinute] = useState(0);
-  const [second, setSecond] = useState(0);
+  const [date, setDate] = useState<Date>();
 
   function refreshClock() {
     const currentDate = new Date();
-    setHour(currentDate.getHours());
-    setMinute(currentDate.getMinutes());
-    setSecond(currentDate.getSeconds());
+    setDate(currentDate);
   }
 
   useEffect(() => {
-    if (window !== undefined) {
-      const timerId = setInterval(refreshClock, 1000);
-      return () => {
-        clearInterval(timerId);
-      };
-    }
+    const timerId = setInterval(refreshClock, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
   }, []);
 
   const clockClassName =
     "rounded-box flex flex-col bg-base-100 p-2 text-neutral-content shadow-md";
 
   const wordClassName = "text-xs";
-  return (
-    <>
-      <div className="mx-auto flex gap-2 pt-2 text-center sm:mx-0">
-        <div className={clockClassName}>
-          <span className="countdown font-mono text-4xl">
-            <span
-              style={
-                {
-                  "--value": hour == 0 ? 0 : hour % 12 || 12,
-                } as React.CSSProperties
-              }
-            />
-          </span>
-          <span className={wordClassName}>hours</span>
+
+  if (date) {
+    return (
+      <>
+        <div className="mx-auto flex gap-2 pt-2 text-center sm:mx-0">
+          <div className={clockClassName}>
+            <span className="countdown font-mono text-4xl">
+              <span
+                style={
+                  {
+                    "--value": date.getHours() % 12 || 12,
+                  } as React.CSSProperties
+                }
+              />
+            </span>
+            <span className={wordClassName}>hours</span>
+          </div>
+          <div className={clockClassName}>
+            <span className="countdown font-mono text-4xl">
+              <span
+                style={
+                  {
+                    "--value": date.getMinutes(),
+                  } as React.CSSProperties
+                }
+              />
+            </span>
+            <span className={wordClassName}>minutes</span>
+          </div>
+          <div className={clockClassName}>
+            <span className="countdown font-mono text-4xl">
+              <span
+                style={
+                  {
+                    "--value": date.getSeconds(),
+                  } as React.CSSProperties
+                }
+              />
+            </span>
+            <span className={wordClassName}>seconds</span>
+          </div>
         </div>
-        <div className={clockClassName}>
-          <span className="countdown font-mono text-4xl">
-            <span style={{ "--value": minute } as React.CSSProperties} />
-          </span>
-          <span className={wordClassName}>minutes</span>
-        </div>
-        <div className={clockClassName}>
-          <span className="countdown font-mono text-4xl">
-            <span
-              style={
-                {
-                  "--value": second,
-                } as React.CSSProperties
-              }
-            />
-          </span>
-          <span className={wordClassName}>seconds</span>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 const ApplicationGrid = () => {
