1ccb0dca9bf58736cb1e589ab8c0be0a21ec56d2
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2023-01-17 13:55:57 +0800

    center hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import LoopingBar from "../components/LoopingBar";
 const Home: NextPage = () => {
   return (
     <div className="mx-auto mt-5 flex max-w-3xl flex-col justify-center">
-      <div className="flex h-[calc(100vh-8rem)] flex-col justify-center gap-5 p-5">
+      <div className="mt-[-5rem] flex h-screen flex-col justify-center gap-5 p-5">
         <h1 className="text-center text-4xl font-bold sm:text-6xl">
           <span className="text-primary">Simplify</span> and{" "}
           <span className="text-secondary">Organize</span>
@@ -17,13 +17,13 @@ const Home: NextPage = () => {
           <Clock />
         </div>
       </div>
-      <div className="flex h-[calc(100vh-8rem)] flex-col justify-center gap-5 p-5">
+      <div className="flex h-screen flex-col justify-center gap-5 p-5">
         <div className="flex basis-1/2 flex-col justify-center gap-2">
           <h2 className="font-bold uppercase">Money Track</h2>
-          <h1 className="text-4xl font-bold text-primary">
+          <h1 className="text-3xl font-bold text-primary sm:text-4xl">
             Track Your Finance
           </h1>
-          <p className="text-xl">
+          <p className="text-md sm:text-xl">
             The app also features a user-friendly interface for tracking and
             managing your finances. With clear and concise categories for income
             and expenses, it&apos;s easy to stay on top of your spending and
@@ -35,10 +35,10 @@ const Home: NextPage = () => {
         </div>
         <div className="flex basis-1/2 flex-col justify-center gap-2">
           <h2 className="font-bold uppercase">Chef Burp</h2>
-          <h1 className="text-4xl font-bold text-primary">
+          <h1 className="text-3xl font-bold text-primary sm:text-4xl">
             Community-Based Recipe Platform
           </h1>
-          <p className="text-xl">
+          <p className="text-md sm:text-xl">
             Our recipe app is built on a strong community foundation. Users can
             share and edit their own recipes, as well as rate the recipes
             submitted by other users.
