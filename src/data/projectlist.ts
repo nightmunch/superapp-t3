03aecc9e66389427a922e478a3c5ea07439f5009
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2023-01-18 13:44:52 +0800

    update aboutme and swap page

---
+++ src/data/projectlist.ts
@@ -0,0 +1,24 @@
+type ProjectProps = {
+  title: string;
+  description: string;
+  github?: string;
+  link?: string;
+  skills: string[];
+};
+
+export const ProjectList: ProjectProps[] = [
+  {
+    title: "SuperApp",
+    description:
+      "An all-for-one single application that contains many others mini applications to solve users need!",
+    github: "https://github.com/nightmunch/superapp-t3",
+    link: "https://superapp.nightmunch.com/",
+    skills: ["Next.JS", "Typescript", "TailwindCSS", "MongoDB"],
+  },
+  {
+    title: "Digital Wedding Card",
+    description: "Personal wedding card for my future wedding InshaAllah",
+    github: "https://github.com/nightmunch/digital-wedding-card",
+    skills: ["Next.JS", "Typescript", "TailwindCSS", "Supabase"],
+  },
+];
--- src/pages/aboutme.tsx
+++ src/pages/aboutme.tsx
@@ -1,122 +1,154 @@
+import type { NextPage } from "next";
 import Image from "next/image";
-import { FaLinkedin, FaInstagram, FaGithub, FaEnvelope } from "react-icons/fa";
-export default function AboutMe() {
+import { Typewriter } from "react-simple-typewriter";
+
+import { BsTerminal } from "react-icons/bs";
+import { FaGithub, FaRobot } from "react-icons/fa";
+import { FiExternalLink } from "react-icons/fi";
+import { AiOutlineFolder } from "react-icons/ai";
+import { ProjectList } from "../data/projectlist";
+
+const AboutMe: NextPage = () => {
   return (
-    <>
-      <div className="card bg-neutral text-neutral-content shadow-md">
-        <div className="card-body">
-          <div className="flex flex-col sm:flex-row sm:gap-10 md:m-auto xl:w-1/2">
-            <div className="flex">
-              <div className="my-auto">
-                <h1 className="text-lg">Hello!</h1>
-                <h2 className="text-3xl font-semibold text-primary">
-                  I&apos;m Shahrin
-                </h2>
-                <h2 className="text-lg">
-                  Web Developer & Deep Learning Enthusiast
-                </h2>
-              </div>
-            </div>
-            <div className="flex-none pt-6 sm:w-64 sm:pt-0">
-              <Image
-                src="/me.jpg"
-                alt="Picture of Shahrin"
-                width={1000}
-                height={1000}
-                className="rounded-full"
+    <div className="mx-auto mt-5 flex max-w-4xl flex-col justify-center gap-10 px-5 sm:px-0">
+      <div className="flex max-w-3xl flex-col sm:flex-row sm:gap-20 md:m-auto">
+        <div className="flex">
+          <div className="my-auto">
+            <h1 className="text-lg">Hello, I&apos;m</h1>
+            <h2 className="text-4xl font-bold text-primary">Shahrin Amin</h2>
+            <h2 className="text-xl font-bold text-secondary">
+              <Typewriter
+                words={["Full Stack Developer", "Deep Learning Enthusiast"]}
+                loop={0}
               />
+            </h2>
+            <h2 className="text-md">
+              Building cutting-edge web applications and exploring the
+              possibilities of Deep Learning
+            </h2>
+          </div>
+        </div>
+        <div className="flex-none pt-6 sm:w-64 sm:pt-0">
+          <Image
+            src="/me.jpg"
+            alt="Picture of Shahrin"
+            width={1000}
+            height={1000}
+            className="rounded-full"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col justify-center gap-10">
+        <h1 className="text-center text-xl font-bold text-primary">
+          Technical Skills & Programming Language
+        </h1>
+        <div className="grid grid-cols-1 divide-y divide-base-content sm:grid-cols-2 sm:divide-x sm:divide-y-0">
+          <div className="flex flex-col items-center justify-center gap-2 py-5 sm:px-10">
+            <div className="rounded-full bg-primary p-3">
+              <BsTerminal className="text-3xl text-base-100" />
             </div>
+            <h1 className="text-center text-lg font-bold text-secondary">
+              Full Stack Development
+            </h1>
+            <p className="text-center">
+              I am passionate about web development and enjoy the challenge of
+              creating dynamic and interactive websites. I am constantly
+              learning and seeking to improve my skills in this area.
+            </p>
+            <h2 className="text-md mt-3 text-center font-semibold text-secondary">
+              Language/Framework
+            </h2>
+            <p className="text-center">
+              React, Next.JS, Typescript, TailwindCSS, MongoDB, PHP, Laravel,
+              Bootstrap 5, MariaDB, HTML 5, CSS, Javascript, Vue
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2 py-5 sm:px-10">
+            <div className="rounded-full bg-primary p-3">
+              <FaRobot className="text-3xl text-base-100" />
+            </div>
+            <h1 className="text-center text-lg font-bold text-secondary">
+              Data Science & Artificial Intelligence
+            </h1>
+            <p className="text-center">
+              I am passionate about data science and enjoy working with large
+              and complex datasets. I am constantly learning and seeking to
+              improve my skills in this area.
+            </p>
+            <h2 className="text-md mt-3 text-center font-semibold text-secondary">
+              Language/Framework
+            </h2>
+            <p className="text-center">
+              Python, Pandas, Numpy, Sklearn, TensorFlow
+            </p>
           </div>
         </div>
       </div>
-      <div className="card bg-neutral text-neutral-content shadow-md">
-        <div className="card-body">
-          <h1 className="text-center text-xl font-semibold text-primary">
-            My socials
-          </h1>
-          <div className="m-auto mt-3 flex flex-row gap-3">
-            <a href="https://www.instagram.com/nightmunch/?theme=dark">
-              <FaInstagram className="text-3xl hover:text-primary" />
-            </a>
-            <a href="https://www.linkedin.com/in/shahrinamin-2703/">
-              <FaLinkedin className="text-3xl hover:text-primary" />
-            </a>
-            <a href="https://github.com/nightmunch">
-              <FaGithub className="text-3xl hover:text-primary" />
-            </a>
-            <a href="mailto:shahrinamin.my@gmail.com">
-              <FaEnvelope className="text-3xl hover:text-primary" />
-            </a>
-          </div>
+      <div className="flex flex-col justify-center gap-10">
+        <h1 className="text-center text-xl font-bold text-primary">Projects</h1>
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-3">
+          {ProjectList.map((project) => (
+            <ProjectCard
+              key={project.title}
+              github={project.github}
+              link={project.link}
+              title={project.title}
+              description={project.description}
+              skills={project.skills}
+            />
+          ))}
         </div>
       </div>
-      <div className="card bg-neutral text-neutral-content shadow-md">
-        <div className="card-body">
-          <h1 className="text-center text-xl font-semibold text-primary">
-            About Me
-          </h1>
-          <p className="mt-3">
-            As a passionate software developer, I am dedicated to creating
-            high-quality and effective software solutions. I am constantly
-            learning and seeking to improve my skills, and I have a strong
-            desire to stay up-to-date with the latest technologies and best
-            practices in the field. I am a creative problem-solver and enjoy the
-            challenge of tackling complex technical issues. In addition, I am
-            able to work well in a team environment and am always willing to
-            lend a helping hand to my colleagues. Overall, my passion for
-            software development shines through in everything I do, and I am a
-            valuable asset to any organization.
-            <span className="font-semibold text-primary md:mt-2 md:block md:text-center">
-              &quot;Jack of all trades, master of some&quot;.
-            </span>
-          </p>
-        </div>
-      </div>
-      <div className="card bg-neutral text-neutral-content shadow-md">
-        <div className="card-body">
-          <h1 className="text-center text-xl font-semibold text-primary">
-            Skills
-          </h1>
-          <div className="mt-3 flex flex-col gap-3 md:flex-row">
-            <div className="card bg-base-100">
-              <div className="card-body">
-                <h1 className="text-center text-lg font-semibold text-secondary">
-                  Web Development
-                </h1>
-                <p>
-                  I am passionate about web development and enjoy the challenge
-                  of creating dynamic and interactive websites. I am constantly
-                  learning and seeking to improve my skills in this area.
-                </p>
-                <h2 className="text-md mt-3 text-center font-semibold text-secondary">
-                  Language/Framework
-                </h2>
-                <p className="text-center">
-                  HTML, CSS, Javascript, PHP, Laravel, React, Typescript
-                </p>
-              </div>
-            </div>
-            <div className="card bg-base-100">
-              <div className="card-body">
-                <h1 className="text-center text-lg font-semibold text-secondary">
-                  Data Science & Artificial Intelligence
-                </h1>
-                <p>
-                  I am passionate about data science and enjoy working with
-                  large and complex datasets. I am constantly learning and
-                  seeking to improve my skills in this area.
-                </p>
-                <h2 className="text-md mt-3 text-center font-semibold text-secondary">
-                  Language/Framework
-                </h2>
-                <p className="text-center">
-                  Python, Pandas, Numpy, Sklearn, TensorFlow
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
-}
+};
+
+export default AboutMe;
+
+const ProjectCard = ({
+  github,
+  link,
+  title,
+  description,
+  skills,
+}: {
+  github?: string;
+  link?: string;
+  title: string;
+  description: string;
+  skills: string[];
+}) => {
+  return (
+    <div className="group flex flex-col gap-5 bg-base-200 p-5 transition hover:-translate-y-2">
+      <div className="flex items-center">
+        <AiOutlineFolder className="text-4xl text-secondary" />
+        <div className="grow"></div>
+        <div className="flex gap-2">
+          {github && (
+            <a href={github} title="Github">
+              <FaGithub className="text-xl hover:text-primary" />
+            </a>
+          )}
+          {link && (
+            <a href={github} title="External Link">
+              <FiExternalLink className="text-xl hover:text-primary" />
+            </a>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-lg font-bold group-hover:text-primary">{title}</h1>
+        <p className="text-sm text-base-content text-opacity-80">
+          {description}
+        </p>
+        <div className="flex flex-wrap gap-2 group-hover:text-primary">
+          {skills.map((skill) => (
+            <h3 key={skill} className="text-xs">
+              {skill}
+            </h3>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
