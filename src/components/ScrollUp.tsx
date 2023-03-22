b2ed60cfb10d52b65f4d55bff9130ecac7ae1621
nightmunch <shahrinamin.my@gmail.com>
2023-03-22 09:31:20 +0800

    attempt to scrolltotop

---
+++ src/components/ScrollUp.tsx
@@ -0,0 +1,9 @@
+const isBrowser = () => typeof window !== "undefined"; //The approach recommended by Next.js
+function scrollToTop() {
+  if (!isBrowser()) return;
+  setTimeout(() => {
+    window.document.body.scrollIntoView({ behavior: "smooth" });
+  }, 500);
+}
+
+export default scrollToTop;
